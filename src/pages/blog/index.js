4de021931751af79e379b6577567e445f5e8bcc5
Guillaume Bogard <hey@guillaumebogrd.dev>
2019-05-19 11:46:28 +0200

    Improve blog title

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,21 +7,8 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
+        <div className="blog-title">
+          <h1 className="container has-text-weight-bold is-size-1">
             Dernieres nouvelles
           </h1>
         </div>
